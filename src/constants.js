16bdfbf0f316513d60456150d0d15d0fa4df953c
Artem Udovichenko <artemudovichenko90@gmail.com>
2022-08-01 17:17:36 +0300

    Add file with constants

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTSTANTS = {
+  URL: {
+    HOME: '/',
+    LOGIN: '/login',
+    SIGN_UP: '/signup'
+  },
+  RADIO_BUTTON: {
+    BUYER: 'buyer',
+    SELLER: 'seller'
+  }
+}
+
+export default CONTSTANTS;
