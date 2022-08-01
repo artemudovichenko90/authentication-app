e5c1ae199b1eb6205f561223d51b67d436d356d9
Artem Udovichenko <artemudovichenko90@gmail.com>
2022-08-01 17:20:48 +0300

    Add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './page/Home';
+import Login from './page/Login';
+import SignUp from './page/SignUp';
+import styles from './App.module.scss';
+import CONTSTANTS from './constants';
+const { HOME, LOGIN, SIGN_UP } = CONTSTANTS.URL;
 
-function App() {
+const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <div className={styles.container}>
+        <Header />
+        <Routes>
+          <Route path={HOME} element={<Home />} />
+          <Route path={LOGIN} element={<Login />} />
+          <Route path={SIGN_UP} element={<SignUp />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
