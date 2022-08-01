f9b4a21ca0dda180173cd096dcaada73a569a46f
Artem Udovichenko <artemudovichenko90@gmail.com>
2022-08-01 17:16:30 +0300

    Add login component

---
+++ src/components/forms/LoginForm/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Form, Formik } from 'formik';
+import { SCHEMA_LOGIN } from '../../../utils/schemaValidation';
+import Input from '../Input';
+import Submit from '../../Submit/index';
+import styles from './LoginForm.module.scss';
+
+const initialValues = { email: '', password: '' };
+const LoginForm = (props) => {
+  const onSubmit = (values, formikBag) => {
+    formikBag.resetForm();
+  }
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={SCHEMA_LOGIN}
+      onSubmit={onSubmit}>
+      {
+        (formikProps) => {
+          return (
+            <Form className={styles.form}>
+              <Input className={styles.form__input} name="email" placeholder="Enter email" />
+              <Input className={styles.form__input} name="password" placeholder="Enter password" />
+              <Submit name='login' />
+            </Form>
+          );
+        }
+      }
+    </Formik>
+  );
+}
+
+export default LoginForm;
