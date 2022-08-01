6115e72beb994b1dd38102060fe1d14e76618762
Artem Udovichenko <artemudovichenko90@gmail.com>
2022-08-01 17:20:17 +0300

    Add Home and Login pages

---
+++ src/page/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LoginForm from './../components/forms/LoginForm';
+import Title from './../components/Title';
+import styles from './Login.module.scss';
+
+const Login = () => {
+  return (
+      <article className={styles.login}>
+        <Title className={styles.login__title}>
+          Login to your account
+        </Title>
+        <LoginForm />
+      </article>
+  );
+}
+
+export default Login;
