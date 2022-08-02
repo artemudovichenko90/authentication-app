f44d0c6365e0f3be7973eb18edc3f03a16014eb2
Artem Udovichenko <artemudovichenko90@gmail.com>
2022-08-02 16:31:41 +0300

    Refactor error hint

--- src/utils/schemaValidation.js
+++ src/utils/schemaValidation.js
@@ -1,10 +1,10 @@
 import * as Yup from 'yup';
 
-export const SCHEMA_NAME = Yup.string().matches(/^[A-Z][a-z]{1,20}$/, 'Must be latin letter').required();
+export const SCHEMA_NAME = Yup.string().matches(/^[A-Z][a-z]{1,20}$/, 'Incorrect name').required('Required field');
 
-export const SCHEMA_EMAIL = Yup.string().email('Enter correct email').required();
+export const SCHEMA_EMAIL = Yup.string().email('Incorrect email').required('Required field');
 
-export const SCHEMA_PASSWORD = Yup.string().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,36}$/, 'Enter correct password').required()
+export const SCHEMA_PASSWORD = Yup.string().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,36}$/, 'Incorrect password').required('Required field')
 
 export const SCHEMA_SIGN_UP = Yup.object({
   firstName: SCHEMA_NAME,
